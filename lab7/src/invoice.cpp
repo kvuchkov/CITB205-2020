31882a42c002d71a889e85168d07ef22aa0eb34b
Kiril Vuchkov <kiril.vuchkov@uber.com>
2020-04-19 15:09:57 +0300

    Lab 7 solved

--- lab7/src/invoice.cpp
+++ lab7/src/invoice.cpp
@@ -3,6 +3,14 @@
 const double TAX = 0.1; // 10% flat tax rate
 
 void Invoice::add(const Product *product, int quantity) {
+    
+    for (int i=0; i < items.size(); i++) { // using for (auto item : items) will make a copy of item[i], whereas we want to update it
+        if (items[i].getProductID() == product->getID()) {
+            items[i].setQuantity(items[i].getQuantity() + quantity);
+            return;
+        }
+    }
+
     Item item(product, quantity);
     items.push_back(item);
 }
